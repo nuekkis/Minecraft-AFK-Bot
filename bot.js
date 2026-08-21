0eb3445214a8c7bdaf82ce948402fcd6c98ee570
aayushrautela0001-gif <aayushrautela0001@gmail.com>
2026-08-21 14:43:12 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,9 @@ const mineflayer = require('mineflayer');
 const config = require('./config.json');
 
 const bot = mineflayer.createBot({
-  host: config.serverHost,
-  port: config.serverPort,
-  username: config.botUsername,
+  host: config.timedbat2963.aternos.me,
+  port: config.26352,
+  username: config.afk 12q,
   auth: 'offline',
   version: false,
   viewDistance: config.botChunk
